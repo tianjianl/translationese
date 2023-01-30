d354a46d23df73fe50b47546ba824c9859df8488
Tianjian Li <litianjian_1998@sina.com>
2023-01-29 21:04:26 -0500

    initial commit

--- finetune_xlmr.py
+++ finetune_xlmr.py
@@ -1,22 +1,23 @@
 import torch
 import numpy as np
 import pandas as pd
-
-from utils import data_preprocess 
-
+import torch.nn as nn
+import torch.nn.funtional as F
 from torch import cuda
 from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
 
 from transformers import XLMRobertaTokenizerFast, XLMRobertaModel
 
-class CustomDataset(Dataset):
+from utils import data_to_df 
+
+class CustomClassificationDataset(Dataset):
 
     def __init__(self, dataframe, tokenizer, max_len):
         self.tokenizer = tokenizer
         self.data = dataframe
         self.max_len = max_len
         self.input = self.data.src
-        self.prediction = self.data.tgt
+        self.label = self.data.label
 
     def __len__(self):
         return len(self.src)
@@ -24,25 +25,119 @@ class CustomDataset(Dataset):
     def __getitem__(self, index):
         src = str(self.src[index])
         src = ' '.join(src.split())
+        source = self.tokenizer.batch_encode_plus([src], max_length= self.source_len, pad_to_max_length=True,return_tensors='pt')
+        source_ids = source['input_ids'].squeeze()
+        source_mask = source['attention_mask'].squeeze()
+        
+        # list of numeric labels 
+        tgt = self.label[index]
 
+        return {
+            'source_ids': source_ids.to(dtype=torch.long), 
+            'source_mask': source_mask.to(dtype=torch.long), 
+            'label': tgt.to(dtype=torch.long)
+        }
 
+class XLMRForSentenceClassification(nn.Module):
+    
+    def __init__(self, xlmr_model, n_class, hidden_size=768):
+        super(XLMRForSentenceClassification, self).__init__()
+        self.W = nn.Parameter(torch.randn(hidden_size, n_class), requires_grad=True)
+        
+    def forward(self, x, x_mask):
+        output = model(input_ids = x, attention_mask = x_mask)
+        output = torch.mean(output.last_hidden_state)
+        output = torch.matmul(output, self.W))
+        x = F.log_softmax(x)
+        return x
 
+ 
+def train(epoch, tokenizer, model, device, loader, optimizer, scheduler=None):
+    
+    start = time.time()
+    for iteration, data in enumerate(loader, 0):
+        x = data['source_ids'].to(device, dtype = torch.long)
+        x_mask = data['source_mask'].to(device, dtyp = torch.long)
+        y = data['label'].to(device, dtype = torch.long)
+        
+        print(f"x = {x}")
+        print(f"x_mask = {x_mask}")
+        print(f"y = {y}")
+        
+        optimizer.zero_grad()
+        y_hat = model(x, x_mask)
+        loss = nn.NLLLoss(y_hat, y)
+        
+        if iteration%50 == 0:
+            print(f'Epoch: {epoch}, Iteration: {iteration}, Loss:  {loss.item()}')
 
+        loss.backward()
+        optimizer.step()
+    
+    end = time.time()
+    print(f'Epoch: {epoch} used {end-start} seconds')
+
+def validate(epoch, tokenizer, model, device, val_loader):
+    
+    model.eval()
+    predictions = []
+    actuals = []
+    total_dev_loss = []
+    start = time.time()
+    with torch.no_grad():
+        for index, data in enumerate(loader, 0):
+            
+            x = data['source_ids'].to(device, dtype = torch.long)
+            x_mask = data['source_mask'].to(device, dtype = torch.long)
+            y = data['label'].to(device, dtype = torch.long)
+
+            y_hat = model(x, x_mask)
+            loss = nn.NLLLoss(y_hat, y)
+            
+            predictions.extend(torch.argmax(y_hat, dim=1))
+            actuals.extend(y)
+            total_dev_loss.extend(loss.item())
+    
+
+    print(f'evaluation used {now-start} seconds')
+    loss = np.mean(total_dev_loss)
+    return predictions, actuals, loss
 
 def main(args):
     
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
     tokenizer = XLMRobertaTokenizerFast.from_pretrained('xlm-roberta-base')
-    model = XLMRobertaModel.from_pretrained("xlm-roberta-base")
-
-    # prepare input
-    text = "Replace me by any text you'd like."
-    encoded_input = tokenizer.batch_encode_plus(text, return_tensors='pt')
-
-    # forward pass
-    output = model(**encoded_input)
-    pooled_output = torch.mean(output.last_hidden_state, dim=1)
-    print(pooled_output.shape)
-
+    xlmr_model = XLMRobertaModel.from_pretrained("xlm-roberta-base")
+    model = XLMRForSentenceClassification(xlmr_model, n_class)
+    
+    acc = evaluate.load("accuracy")
+    loader_params = {
+        'batch_size': args.bs,
+        'shuffle': True,
+        'num_workers': 0
+    }
+    
+    train_dataset = data_to_df(task = args.task, split = 'train')
+    train_dataset = CustomClassificationDataset(train_dataset, tokenizer, args.max_len)
+    training_loader = DataLoader(train_dataset, **loader_params)
+    
+    val_loaders = []
+    for language in val_languages:
+        val_dataset = data_to_df(task = args.task, split = 'val')
+        val_dataset = CustomClassificationDataset(val_dataset, tokenizer, args.max_len)
+        val_loaders.append(Dataloader(val_dataset, **loader_params))
+        
+    for epoch in range(args.epoch):
+        train(epoch, tokenizer, model, device, train_loader, optimizer)
+        for index, val_loader in enumerate(val_loaders):
+            total_acc = []
+            y_hat, y, dev_loss = validate(epoch, tokenizer, model, device, val_loader)       
+            result = acc.compute(references = y, predictions = y_hat)c
+            print(f"epoch = {epoch} | language = {val_languages[index]} | acc = {result['accuracy']}")
+            total_acc.append(result['accuracy'])
+        print(f"epoch = {epoch} | acc = {np.mean(total_acc)}")
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--epoch", default=10, type=int)
--- utils.py
+++ utils.py
@@ -1,8 +1,27 @@
-import os
-import sys
-import numpy as np
+import pandas ad pd
 
+def data_to_df(task, langauge, split):
+    
+    if task == "xnli":
+        filename = f"../xnli/{split}-{language}.tsv"
+        f = open(filename, 'r')
+        source = []
+        labels = []
+        for line in f:
+            cols = line.strip().split('\t')
+            premise = cols[0]
+            hypothesis = cols[1]
+            if cols[2] == 'neutral':
+                label = 0
+            elif cols[2] == 'entailment':
+                label = 1
+            elif cols[2] == 'contradiction':
+                label = 2
+            src.append(premise + " " + hypothesis)
+            labels.append(label)
 
-def data_preprocess(task, size):
-    
-    
+        df = pd.DataFrame({"src": src, "label": label})
+        print(df.head(5)) 
+        return df
+    else:
+        return None
