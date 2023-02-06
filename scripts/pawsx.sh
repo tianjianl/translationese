f02de1e7e5cac5eb47b931a41aab609997954baa
truthbutcher <tli104@jhu.edu>
2023-02-06 21:42:01 +0000

    fixed bug

--- scripts/pawsx.sh
+++ scripts/pawsx.sh
@@ -1,2 +1,2 @@
-#python3 finetune_xlmr.py --task pawsx --lr 0.00002 --epoch 5 --bs 32 --max_len 128 --regularizer ewc
+python3 finetune_xlmr.py --task pawsx --lr 0.00002 --epoch 5 --bs 32 --max_len 128 --regularizer ewc
 python3 finetune_xlmr.py --task pawsx --lr 0.00002 --epoch 5 --bs 32 --max_len 128
