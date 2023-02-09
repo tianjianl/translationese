75835d74c7a75a114288378a620efeadc4d5e2b0
Tianjian Li <litianjian_1998@sina.com>
2023-02-09 13:01:46 -0500

    Update xnli.sh

--- scripts/xnli.sh
+++ scripts/xnli.sh
@@ -1,1 +1,3 @@
-python3 finetune_xlmr.py --task xnli --lr 0.000002 --epoch 5 --bs 32 --max_len 128 
+python3 finetune_xlmr.py --task xnli --lr 0.000005 --epoch 5 --bs 32 --max_len 128 
+python3 finetune_xlmr.py --task xnli --lr 0.000005 --epoch 5 --bs 32 --max_len 128 --regularizer r3f
+python3 finetune_xlmr.py --task xnli --lr 0.000005 --epoch 5 --bs 32 --max_len 128 --regularizer ewc
