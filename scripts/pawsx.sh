346bb0309b63247f45c0cded48a2f985b511c8dd
Tianjian Li <litianjian_1998@sina.com>
2023-02-09 13:01:18 -0500

    Update pawsx.sh

--- scripts/pawsx.sh
+++ scripts/pawsx.sh
@@ -1,3 +1,3 @@
-python3 finetune_xlmr.py --task pawsx --lr 0.000002 --epoch 10 --bs 32 --max_len 128 
-python3 finetune_xlmr.py --task pawsx --lr 0.000002 --epoch 10 --bs 32 --max_len 128 --regularizer r3f
-python3 finetune_xlmr.py --task pawsx --lr 0.000002 --epoch 10 --bs 32 --max_len 128 --regularizer ewc
+python3 finetune_xlmr.py --task pawsx --lr 0.000005 --epoch 10 --bs 32 --max_len 128 
+python3 finetune_xlmr.py --task pawsx --lr 0.000005 --epoch 10 --bs 32 --max_len 128 --regularizer r3f
+python3 finetune_xlmr.py --task pawsx --lr 0.000005 --epoch 10 --bs 32 --max_len 128 --regularizer ewc
