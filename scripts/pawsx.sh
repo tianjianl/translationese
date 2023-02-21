1ad1a1b570be3d62014cd927b4e8bc20287fafb5
truthbutcher <tli104@jhu.edu>
2023-02-21 04:39:37 +0000

    fixed bug in layer-wise importance printing

--- scripts/pawsx.sh
+++ scripts/pawsx.sh
@@ -1,4 +1,4 @@
-python3 finetune_xlmr.py --task pawsx --lr 0.000005 --epoch 5 --bs 32 --max_len 128 
+python3 finetune_xlmr.py --task pawsx --lr 0.000005 --epoch 5 --bs 32 --max_len 128  --plot_params
 #python3 finetune_xlmr.py --task pawsx --lr 0.000005 --epoch 5 --bs 32 --max_len 128 --sage
 
 #python3 finetune_xlmr.py --task pawsx --lr 0.000005 --epoch 5 --bs 32 --max_len 128 --regularizer r3f
